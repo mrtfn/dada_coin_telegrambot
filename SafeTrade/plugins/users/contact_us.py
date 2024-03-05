fca6137369f5b231a4dd1a61b2eedf8c848e1406
Toofan Mohammadi <tfn.mohammadi@gmail.com>
2024-03-05 14:02:46 +0330

    dev(SafeTrade); try to figure message handling

--- SafeTrade/plugins/users/contact_us.py
+++ SafeTrade/plugins/users/contact_us.py
@@ -3,8 +3,6 @@ from SafeTrade import bot
 from SafeTrade.helpers.start_constants import CONTACT_US_REPLAY
 
 
-
-
 @bot.message_handler(func=lambda message: True)
 async def handle_message(message):
     if message.text == "CONTACT_US_BUTTON":
@@ -16,17 +14,3 @@ async def handle_message(message):
         
         # Send a confirmation message to the user
         await bot.send_message(message.chat.id, CONTACT_US_REPLAY)
-        
-        
-        
-        
-# Define a function to handle incoming messages
-async def message_handler(client, message):
-    # Check if the message is a reply to the "Now send your message please." message
-    if message.reply_to_message and message.reply_to_message.text == "Now send your message please.":
-        # Get the user's message
-        user_message = message.text
-        # Save the user's message to the database
-        await saveContactMessage(message.chat.id, user_message)
-        # Send a confirmation message to the user
-        await message.reply_text("Your message has been received. Thank you!")
