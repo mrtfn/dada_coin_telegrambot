c24ce3a30fb37458d2884b9d68b94ed4324c8748
Toofan Mohammadi <tfn.mohammadi@gmail.com>
2024-03-05 13:58:04 +0330

    add(SafeTrade): Contact_us collection(table in sql) and save function of it

--- SafeTrade/database/MongoDB/database.py
+++ SafeTrade/database/MongoDB/database.py
@@ -24,3 +24,16 @@ async def saveUser(user):
 
 #     insert_format = {"date": datetime.now(timezone.utc)}
 #     await db.chats.update_document(chatid, insert_format)
+
+
+async def saveContactMessage(user_id, message):
+    """
+    Save a message from a user in the contact us section.
+    """
+    insert_format = {
+        "user_id": user_id,
+        "message_id": str(uuid.uuid4()),
+        "message": message,
+        "timestamp": datetime.now(timezone.utc)
+    }
+    await db.contact_us.insert_document(insert_format)
